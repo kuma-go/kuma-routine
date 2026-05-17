97a3b419c891237a37673f8281be47ec6465e175
KUMA <carksk2@gmail.com>
2026-05-17 18:56:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "kuma-routine-cache-v20260517-5";
+const CACHE_NAME = "kuma-routine-cache-v20260517-6";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260517-21",
-  "./app.js?v=20260517-21",
+  "./styles.css?v=20260517-22",
+  "./app.js?v=20260517-22",
   "./manifest.json",
   "./pwa-icon.svg",
   "./icon-192.png",
